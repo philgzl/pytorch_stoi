f87f799a5430fbf5a2ca54f92acdbf82239a003f
philgzl <services@philgzl.com>
2023-04-21 19:31:25 +0200

    Skip corrupt files in brever2torch_stoi.py

--- scripts/brever2torch_stoi.py
+++ scripts/brever2torch_stoi.py
@@ -15,7 +15,11 @@ def main():
     dataset = BreverDataset(args.in_path)
     filenames = []
     for i in tqdm(range(len(dataset))):
-        noisy, clean = dataset[i].mean(axis=-2)
+        try:
+            noisy, clean = dataset[i].mean(axis=-2)
+        except:
+            print(f'Could not read mixture {i}, skipping')
+            continue
         filename = f'{out_path}/{i:03d}_16kHz.wav'
         torchaudio.save(filename.replace('.wav', '_est.wav'), noisy.unsqueeze(0), 16000)
         torchaudio.save(filename, clean.unsqueeze(0), 16000)
@@ -23,7 +27,7 @@ def main():
 
         noisy = resample(noisy, 16000, 8000)
         clean = resample(clean, 16000, 8000)
-        filename = f'{out_path}/{i:03d}_8kHz.wav'
+        filename = f'{out_path}/{i:05d}_8kHz.wav'
         torchaudio.save(filename.replace('.wav', '_est.wav'), noisy.unsqueeze(0), 8000)
         torchaudio.save(filename, clean.unsqueeze(0), 8000)
         filenames.append(filename)
