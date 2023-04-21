713b1134e3706e39db7758b593a2fe7cc9d3e0f6
philgzl <services@philgzl.com>
2023-04-21 13:50:00 +0200

    Update np torch compare script

--- tests/np_torch_compare.py
+++ tests/np_torch_compare.py
@@ -12,22 +12,12 @@ import numpy as np
 from pystoi import stoi
 from torch_stoi import NegSTOILoss
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--file_list', type=str, default=None,
-                    help='Text file with all wav files to test. We assume'
-                         'that for each test file s1.wav, s1_est.wav exists.')
-
 
 def main(file_list):
     labels = ['np', 'np_ext', 'pt_vad_ext', 'pt_vad', 'pt_ext', 'pt', 'fs', 'fname']
     files_df = pd.read_csv(file_list)
     stoi_df = pd.DataFrame(columns=labels)
     for i, wav_file in tqdm(enumerate(files_df['filename']), total=len(files_df)):
-        # Read files
-        # if i != 538:
-        #     continue
-        # else:
-        #     import ipdb; ipdb.set_trace()
         try:
             clean, fs = sf.read(wav_file, dtype='float32')
             enh, fs = sf.read(wav_file.replace('.wav', '_est.wav'), dtype='float32')
@@ -38,17 +28,11 @@ def main(file_list):
         line += [fs, wav_file]
         stoi_df.loc[len(stoi_df)] = line
 
-        mix_name = os.path.join('/'.join(wav_file.split('/')[:-1]), 'mix.wav')
-        try:
-            enh, fs = sf.read(mix_name, dtype='float32')
-        except:
-            print("Could not read file ", mix_name)
-            continue
         line = make_line(clean, enh, fs)
         line += [fs, wav_file + '.mix']
         stoi_df.loc[len(stoi_df)] = line
 
-        if i % 250 == 0 and i > 0:
+        if i % 250 == 0 and i > 0 or i == len(files_df) - 1:
             df_name = 'partial{}_df.csv'.format(2*i)
             stoi_df.to_csv(df_name)
     return stoi_df
@@ -67,14 +51,9 @@ def make_line(clean, enh, fs):
     return line
 
 
-def plot_fromdf(df):
-
-    return
-
-
 def do_plot(x, y, data, title=None):
     u = np.arange(1000) / 1000  # To make diagonal
-    fig = plt.figure(figsize=(6, 4))
+    plt.figure(figsize=(6, 4))
     sns.scatterplot(x=x, y=y, data=data)
     plt.xlim(0, 1)
     plt.ylim(0, 1.5)
@@ -83,29 +62,34 @@ def do_plot(x, y, data, title=None):
     plt.plot(u, u)
     plt.title(title)
     filename = title.replace(' ', '').replace(',', '').replace('/', '')
-    plt.savefig('../plots/{}.png'.format(filename), dpi=200)
+    os.makedirs('plots', exist_ok=True)
+    plt.savefig('plots/{}.png'.format(filename), dpi=200)
     plt.close()
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file_list')
+    parser.add_argument('--df')
     args = parser.parse_args()
-    # df = main(args.file_list)
-    df = pd.read_csv('partial56000_df.csv')
-    # plot_fromdf(df)
-    u = np.arange(1000) / 1000  # To make diagonal
+    if args.file_list is not None:
+        df = main(args.file_list)
+    if args.df is not None:
+        df = pd.read_csv(args.df)
+        u = np.arange(1000) / 1000  # To make diagonal
 
-    # 8K plots
-    df8k = df[df['fs'] == 8000.0]
-    df8k = df8k[df8k['np'] != 1e-5]  # Very short utts
+        # 8K plots
+        df8k = df[df['fs'] == 8000.0]
+        df8k = df8k[df8k['np'] != 1e-5]  # Very short utts
 
-    do_plot('np', 'pt_vad', df8k, title="8kHz with VAD")
-    do_plot('np', 'pt', df8k, title="8kHz w/o VAD")
-    do_plot('np_ext', 'pt_vad_ext', df8k, title="8kHz Extended with VAD")
-    do_plot('np_ext', 'pt_ext', df8k, title="8kHz Extended w/o VAD")
+        do_plot('np', 'pt_vad', df8k, title="8kHz with VAD")
+        do_plot('np', 'pt', df8k, title="8kHz w/o VAD")
+        do_plot('np_ext', 'pt_vad_ext', df8k, title="8kHz Extended with VAD")
+        do_plot('np_ext', 'pt_ext', df8k, title="8kHz Extended w/o VAD")
 
-    # 16k plots
-    df16k = df[df['fs'] == 16000.0]
-    do_plot('np', 'pt_vad', df16k, title="16kHz with VAD")
-    do_plot('np', 'pt', df16k, title="16kHz w/o VAD")
-    do_plot('np_ext', 'pt_vad_ext', df16k, title="16kHz Extended with VAD")
-    do_plot('np_ext', 'pt_ext', df16k, title="16kHz Extended w/o VAD")
+        # 16k plots
+        df16k = df[df['fs'] == 16000.0]
+        do_plot('np', 'pt_vad', df16k, title="16kHz with VAD")
+        do_plot('np', 'pt', df16k, title="16kHz w/o VAD")
+        do_plot('np_ext', 'pt_vad_ext', df16k, title="16kHz Extended with VAD")
+        do_plot('np_ext', 'pt_ext', df16k, title="16kHz Extended w/o VAD")
